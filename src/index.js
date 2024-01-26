40344806dc8e8f4bc9d45cabd75b198d3373d587
Tuan PHA <tuanpha.it@gmail.com>
2024-01-26 20:11:47 +0700

    add login

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/css/index.css';
 import App from './App';
+import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Login />
+	</React.StrictMode>
 );
-
