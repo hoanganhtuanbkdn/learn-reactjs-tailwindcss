7b3e754de742fee93bb5dcbf82723851485b2f2a
Tuan PHA <tuanpha.it@gmail.com>
2024-02-21 23:21:15 +0700

    feat: update code

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,10 +1,43 @@
 import Banner from '../components/Banner';
-import Session1 from '../components/Session1';
+import BlogItem from '../components/BlogItem';
+
+import { useEffect, useState } from 'react';
+
 function App() {
+	const [listPosts, setListPosts] = useState([]);
+
+	useEffect(() => {
+		fetch('http://localhost:4000/posts')
+			.then((raw) => raw.json())
+			.then((response) => {
+				setListPosts(response);
+				console.log('response', response);
+			})
+			.catch((error) => {
+				console.log('error', error);
+			});
+
+		//
+	}, []);
+
 	return (
 		<>
 			<Banner title="Product" />
-			<Session1 />
+			<div className="pb-[105px]">
+				<p className="text-[36px] font-bold mb-[8px]">
+					Popular Destinations
+				</p>
+				<p>Vacations to make your experience enjoyable in Indonesia!</p>
+				<div className="mt-[60px] grid grid-cols-4 gap-[30px]">
+					{listPosts.map((item) => (
+						<BlogItem
+							photo={item.photo}
+							item={item}
+							key={item.id}
+						/>
+					))}
+				</div>
+			</div>
 		</>
 	);
 }
