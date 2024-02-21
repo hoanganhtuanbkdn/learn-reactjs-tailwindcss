ad6b1537140399493529dedad1ca5d7458ace1f0
Tuan PHA <tuanpha.it@gmail.com>
2024-02-21 19:29:31 +0700

    feat: add json server

--- src/components/Session1.js
+++ src/components/Session1.js
@@ -1,9 +1,25 @@
 import Photo from '../assets/images/photo1.png';
 import BlogItem from './BlogItem';
 
-import { listPost } from '../constants/posts';
+import { useEffect, useState } from 'react';
 
 function Session1() {
+	const [listPosts, setListPosts] = useState([]);
+
+	useEffect(() => {
+		fetch('http://localhost:4000/posts?_start=0&_limit=4')
+			.then((raw) => raw.json())
+			.then((response) => {
+				setListPosts(response);
+				console.log('response', response);
+			})
+			.catch((error) => {
+				console.log('error', error);
+			});
+
+		//
+	}, []);
+
 	return (
 		<div className="pb-[105px]">
 			<p className="text-[36px] font-bold mb-[8px]">
@@ -11,8 +27,8 @@ function Session1() {
 			</p>
 			<p>Vacations to make your experience enjoyable in Indonesia!</p>
 			<div className="mt-[60px] grid grid-cols-4 gap-[30px]">
-				{listPost.map((item) => (
-					<BlogItem photo={Photo} item={item} key={item.id} />
+				{listPosts.map((item) => (
+					<BlogItem photo={item.photo} item={item} key={item.id} />
 				))}
 			</div>
 		</div>
